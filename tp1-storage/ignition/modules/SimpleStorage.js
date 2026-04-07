31b03b3084c1b9610526c23e4d6e9e5394ebd7cb
walid <walid.hadak@ellabts.com>
2026-04-07 17:16:03 +0200

    modif

---
+++ tp1-storage/ignition/modules/SimpleStorage.js
@@ -0,0 +1,15 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+/**
+ * Module Hardhat Ignition pour SimpleStorage
+ * Gère le déploiement de façon idempotente :
+ * si le contrat est déjà déployé, Ignition ne le redéploie pas
+ */
+const SimpleStorageModule = buildModule("SimpleStorageModule", (m) => {
+  // Déployer SimpleStorage
+  const simpleStorage = m.contract("SimpleStorage");
+
+  return { simpleStorage };
+});
+
+module.exports = SimpleStorageModule;
